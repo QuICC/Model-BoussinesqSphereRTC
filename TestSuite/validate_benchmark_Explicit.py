ba0d9f2f5b7329e9a29315e40088025c0e3cc032
Philippe Marti <philippe.marti@erdw.ethz.ch>
2022-09-29 11:43:59 +0200

    Fix variants

--- TestSuite/validate_benchmark_Explicit.py
+++ TestSuite/validate_benchmark_Explicit.py
@@ -11,7 +11,7 @@ vt.check_setup('OUT_stdout', ref_dir, data_dir, 'Timestepper information', 7)
 
 # Tolerance per max rows
 rows = list(range(0, 101, 10))
-tols = [26, 90, 179, 219, 254, 312, 357, 392, 466, 550, 600]
+tols = [26, 90, 179, 219, 254, 312, 357, 461, 545, 615, 600]
 
 prefixes = ['temperature', 'kinetic']
 spectra = ['l', 'm', 'n']
