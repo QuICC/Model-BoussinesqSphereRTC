2ae1d146dc20f493496aebb6592a435e3c288ea3
Philippe Marti <philippe.marti@erdw.ethz.ch>
2023-11-02 11:41:31 +0100

    Reset tolerances

--- TestSuite/validate_benchmark_Explicit.py
+++ TestSuite/validate_benchmark_Explicit.py
@@ -12,6 +12,7 @@ vt.check_setup('OUT_stdout', ref_dir, data_dir, 'Timestepper information', 7)
 # Tolerance per max rows
 rows = list(range(0, 101, 10))
 tols = [26, 90, 179, 229, 254, 317, 372, 461, 570, 674, 758]
+tols = [0]*len(tols) # reset tolerances
 
 prefixes = ['temperature', 'kinetic']
 spectra = ['l', 'm', 'n']
--- TestSuite/validate_benchmark_Implicit.py
+++ TestSuite/validate_benchmark_Implicit.py
@@ -12,6 +12,7 @@ vt.check_setup('OUT_stdout', ref_dir, data_dir, 'Timestepper information', 7)
 # Tolerance per max rows
 rows = list(range(0, 101, 10))
 tols = [26, 90, 179, 219, 254, 312, 357, 392, 466, 550, 600]
+tols = [0]*len(tols) # reset tolerances
 
 prefixes = ['temperature', 'kinetic']
 spectra = ['l', 'm', 'n']
