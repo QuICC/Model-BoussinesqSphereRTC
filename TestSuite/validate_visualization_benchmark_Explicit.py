2345575ad68e7cd3542723e73b57fefe04d600f5
Philippe Marti <philippe.marti@eaps.ethz.ch>
2025-11-28 13:13:56 +0200

    Tune ULP

--- TestSuite/validate_visualization_benchmark_Explicit.py
+++ TestSuite/validate_visualization_benchmark_Explicit.py
@@ -12,10 +12,10 @@ out = vt.checkHdf5("visState0000.hdf5", ref_dir, data_dir, physicals, 0)
 results.append(out[0])
 
 tids = [0, 1, 2, 3]
-tols = [350000, 112000, 6800, 11300]
+tols = [50, 50, 50, 50]
 datasets = ['temperature/temperature', 'velocity/velocity_r', 'velocity/velocity_theta', 'velocity/velocity_phi']
 for tid, t, ds in  zip(tids, tols, datasets):
-    results.append(vt.hdf5Test(*out[1], ds, tid, tol = t, perrow = True, percol = True, threshold = 2e-12))
+    results.append(vt.hdf5Test(*out[1], ds, tid, tol = t, peraxis = (0,1,2), threshold = 2e-12))
 
 # Output test summary
 vt.printSummary(results, tids, reftol = tols)
